cfec2f1cae86db6269cfb6d974c3fa7fcd9c4eb3
SnrNotHere16 <aquineda16@hotmail.com>
2021-06-13 23:55:40 -0700

    Add files via upload
    Added comments to ex2.py

--- SimpleExamples/DeterminantImport/ex2.py
+++ SimpleExamples/DeterminantImport/ex2.py
@@ -1,3 +1,6 @@
+#The following file calculates the determinant of a 2x2 matrix while using 
+#functions from file ex1.py 
+#Import the functions from file ex1.py
 import ex1
 mat = ex1. inputMat()
 ex1.printMat(mat)
