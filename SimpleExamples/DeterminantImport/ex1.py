0dd975f1f83061df88ed921f7b4cece2efab07f3
SnrNotHere16 <aquineda16@hotmail.com>
2021-06-13 23:52:36 -0700

    Add files via upload
    Added comments

--- SimpleExamples/DeterminantImport/ex1.py
+++ SimpleExamples/DeterminantImport/ex1.py
@@ -1,4 +1,5 @@
-#The determinant of a 2x2 matrix using functions 
+#The functions for calcuating the determinant of a 2x2 matrix
+#Takes the user input a 2X2 matrix 
 def inputMat(): 
   mat = []
   mat = [0,0,0,0]
@@ -7,8 +8,10 @@ def inputMat():
   mat[2] =  int(input("Enter element  2\n") )
   mat[3] =  int(input("Enter element  3\n") )
   return mat 
+#Prints the 2x2 matrix 
 def printMat(mat):
     print ("\r", mat[0], " " , mat[1], "\n", mat[2], " ", mat[3])
+#Calculates the determinant of a 2x2 matrix 
 def det(mat): 
     return (mat[0]*mat[3])-(mat[1]*mat[2])
 
