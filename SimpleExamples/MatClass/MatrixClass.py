3af9a4f92d37a9bb34ccd0c1963a867cb03bda17
SnrNotHere16 <aquineda16@hotmail.com>
2021-06-15 20:15:20 -0700

    Added addition operator overload

--- SimpleExamples/MatClass/MatrixClass.py
+++ SimpleExamples/MatClass/MatrixClass.py
@@ -6,9 +6,14 @@ class Matrix(object):
         self.E10 = E10
         self.E11 = E11
 
-    def printMat(self):
-        print ("\r", self.E00, " " , self.E01, "\n", self.E10, " ", self.E11)
-
+    def __str__(self):
+        return "\n {0} {1}\n {2} {3}\n".format(self.E00,self.E01, self.E10, self.E11)
+    def __add__(self,other): 
+        E00 = self.E00 + other.E00 
+        E01 = self.E01+other.E01
+        E10 = self.E10+other.E10 
+        E11 = self.E11+other.E11
+        return Matrix(E00, E01, E10, E11) 
     def det(self):
         return (self.E00*self.E11)-(self.E01*self.E10)
 
--- SimpleExamples/MatClass/MatrixClassExample.py
+++ SimpleExamples/MatClass/MatrixClassExample.py
@@ -9,6 +9,11 @@ def inputMat():
     mat[3] =  int(input("Enter element  3\n") )
     return mat
 mat = inputMat()
-test_mat = Matrix(mat[0], mat[1], mat[2], mat[3]) 
-test_mat.printMat()
+test_mat = Matrix(mat[0], mat[1], mat[2], mat[3])
+test_mat2 = Matrix(1,1,1,1) 
+print("A = \n",test_mat) 
 print("Det(A) = ", test_mat.det())
+print("B = \n", test_mat2) 
+print("Det(B) = ", test_mat2.det())
+print("A+B = \n", test_mat+test_mat2) 
+
