b865613351c44aa491417e264f9c56c51228250b
SnrNotHere16 <aquineda16@hotmail.com>
2021-06-24 12:01:33 -0700

    Add files via upload

--- SimpleExamples/MatClassForloop/MatClass3x3Forloop.py
+++ SimpleExamples/MatClassForloop/MatClass3x3Forloop.py
@@ -1,15 +1,20 @@
-#The following is a class for a matrix using a for loop 
+#The following is a class for a nxn matrix using a for loop.  
 import math 
 class Matrix(object): 
     #The constructor of the Matrix 
+    #The constructor will always create an nxn matrix.
     def __init__(self, matrix):
        self.mat = matrix
+       #The following statement checks if the list is NOT an nxn matrix. 
+       #In the case it is not, it add zeros to the matrix to make it an nxn matrix. 
        if math.sqrt(len(self.mat))-math.floor(math.sqrt(len(self.mat))) != 0:
            for x in range (pow(math.ceil(math.sqrt(len(self.mat))),2)-len(self.mat)): 
                self.mat.append(0) 
+    #The following function prints out the matrix in an nxn form 
     def __str__(self):
         matStr = "\n"        
         root = math.sqrt(len(self.mat))
+        #Concatenates the appropiate elements to the string. 
         for x in range(len(self.mat)): 
             if x%root==0: 
                 matStr+='\n' 
@@ -18,16 +23,19 @@ class Matrix(object):
                  matStr+=" " 
                  matStr+=str(self.mat[x])
         return matStr
+    #The following function is for adding two matrices together. A+B
     def __add__(self,other): 
         matSum = []
         for x in range(len(self.mat)): 
             matSum.append(self.mat[x]+other.mat[x])
         return Matrix(matSum)
+    #The following function is for subtracting two matrices together.A-B 
     def __sub__(self,other): 
         matDif = []
         for x in range(len(self.mat)): 
            matDif.append( self.mat[x]-other.mat[x])
         return Matrix(matDif)
+    #The following function returns a scalar multiple of the matrix c*A
     def scalarMul(self,c): 
         matScal = [] 
         for x in range(len(self.mat)): 
--- SimpleExamples/MatClassForloop/MatrixClassForloopEx.py
+++ SimpleExamples/MatClassForloop/MatrixClassForloopEx.py
@@ -3,8 +3,10 @@ from  MatClass3x3Forloop  import Matrix
 
 mat = [8,7,6,5,4,3,2,1,0] 
 test_mat = Matrix(mat)
+print("A =")
 print(test_mat)
 mat2 = [2,1,2] 
+print("\nB =")
 test_mat2 = Matrix(mat2)
 print(test_mat2)
 mat3= [1,2,3,4,5,6,7,8,9]
@@ -12,8 +14,9 @@ test_mat3 =Matrix( mat3)
 print(test_mat3)
 print("\nA+C =")
 print(test_mat3+test_mat)
-print("\n A-C = ")
+print("\nA-C = ")
 print(test_mat-test_mat3)
 print(test_mat)
+print("\n5*A = ")
 print(test_mat.scalarMul(5))
 
